538c952b8c443cb01a871823644a826e0c3879d8
34ruby <ixia5027@gmail.com>
2022-08-19 02:46:29 +0900

    업로드 및 삭제

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
-
+import { useState } from "react";
 import './App.css';
 
 function App() {
-  const onSelectFile = (event) => {
-    const selectedFiles = event.target.files;
-    console.log(selectedFiles);
+  const [uploadImages, setImages] = useState([]);
+  const importFile = (event) => {
+
+    const uploadFiles = event.target.files;
+    const filesArray = Array.from(uploadFiles);
+
+    const imagesArray = filesArray.map((file) => {
+      return URL.createObjectURL(file);
+     })
+
+     setImages(imagesArray)
   }
 
   return (
     <section>
+      <h3>현재 업로드 된 이미지 수 : {uploadImages.length} 개</h3> <hr/>
+
       <label htmlFor="image">
-        이미지 추가하기
+        이미지 추가
       </label>
       <br/>
-      <input type="file" name="'images" onChange={onSelectFile} multiple accept="image/png, image/gif, image/jpg" />
+
+      <input type="file" name="images" onChange={importFile} multiple accept="image/png, image/gif, image/jpg" />
+      <div className="images">
+      {uploadImages && uploadImages.map((image,index) => {
+        return (
+          <div key={image} className="image">
+            <img src={image} height="200" />
+            <button onClick={() => setImages(uploadImages.filter((e) => e !== image))}>삭제!</button>
+            <p>{index+1}번 사진</p>
+          </div>
+        )
+      })}
+      </div>
     </section>
   );
 }
